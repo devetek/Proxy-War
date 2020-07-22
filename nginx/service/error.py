6402c1eb1f9c7e482956ad7193957436c89608c6
Nedya Prakasa <nedya.prakasa@tokopedia.com>
2020-07-22 16:13:26 +0700

    feat(repo): fixing unused code
    fixing unused code

--- nginx/service/error.py
+++ nginx/service/error.py
@@ -1,12 +1,28 @@
-from flask import Response, Flask, request
-from helpers import graphs, count_process, prometheus_client, time
+from flask import Response, Flask, request, make_response
 
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-  return 'Hi guest, are you lost in page %s' % path
+  is_error = 'False'
+  code_error = 500
+
+  if 'X-Header-Error' in request.headers:
+    is_error = request.headers['X-Header-Error']
+
+  if 'X-Header-Status' in request.headers:
+    code_error = request.headers['X-Header-Status']
+
+  response = make_response('Hi guest, are you lost in page %s, with error status %s' % (path, code_error))
+  response.status_code = int(code_error)
+  response.headers['X-App-Name'] = 'tkp-error-app'
+
+  # Default error handler simulation
+  if is_error == 'True':
+    response.status_code = 503
+
+  return response
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8081, debug=True)
--- nginx/service/main.py
+++ nginx/service/main.py
@@ -1,22 +1,37 @@
-from flask import Response, Flask, request
-from helpers import graphs, count_process, prometheus_client, time
+from flask import Response, Flask, request, make_response
 
 app = Flask(__name__)
-
-
-@app.route("/metrics")
-def requests_count():
-    res = []
-    for _,value in graphs.items():
-      res.append(prometheus_client.generate_latest(value))
-    
-    return Response(res, mimetype="text/plain")
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    count_process()
-    return 'Hi guest, welcome to %s' % path
+  return location_response(path)
+
+def location_response(path):
+  response = make_response('Hi guest, you\'re in page %s' % path)
+  response.status_code = 200
+  response.headers['X-App-Name'] = 'tkp-backend-app'
+
+  if path == "error-400":
+    response.status_code = 400
+  elif path == "error-401":
+    response.status_code = 401
+  elif path == "error-403":
+    response.status_code = 403
+  elif path == "error-404":
+    response.status_code = 404
+  elif path == "error-500":
+    response.status_code = 500
+  elif path == "error-502":
+    response.status_code = 502
+  elif path == "error-503":
+    response.status_code = 503
+  elif path == "error-504":
+    response.status_code = 504
+
+  return response
+    
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080, debug=True)
